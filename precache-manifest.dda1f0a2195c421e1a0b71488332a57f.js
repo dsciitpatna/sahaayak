7c996918e84595099ba9ff4f6b14c7276f6aef1c
Harsh <29hj29@gmail.com>
2019-03-05 01:07:26 +0530

    Updates

---
+++ precache-manifest.dda1f0a2195c421e1a0b71488332a57f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "481fc3243333d31ce30d",
+    "url": "/sahaayak/src/static/js/runtime~main.481fc324.js"
+  },
+  {
+    "revision": "a468d873abcfa9988de6",
+    "url": "/sahaayak/src/static/js/main.a468d873.chunk.js"
+  },
+  {
+    "revision": "c6745dca6230be67b3ab",
+    "url": "/sahaayak/src/static/js/2.c6745dca.chunk.js"
+  },
+  {
+    "revision": "a468d873abcfa9988de6",
+    "url": "/sahaayak/src/static/css/main.117654d0.chunk.css"
+  },
+  {
+    "revision": "c6745dca6230be67b3ab",
+    "url": "/sahaayak/src/static/css/2.6592ac5d.chunk.css"
+  },
+  {
+    "revision": "84a7d8da146c133ccb311b483e986092",
+    "url": "/sahaayak/src/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sahaayak/precache-manifest.25500dfe6b401baf7adc879fe36de43f.js"
+  "/sahaayak/src/precache-manifest.dda1f0a2195c421e1a0b71488332a57f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sahaayak/index.html", {
+workbox.routing.registerNavigationRoute("/sahaayak/src/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
