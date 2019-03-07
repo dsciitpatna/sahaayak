51714d9d1f594418c1cb8c1be8669ce56bed7636
Harsh <29hj29@gmail.com>
2019-03-08 02:27:38 +0530

    Add navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import {
-  Layout, Menu, Icon,
+  Layout, Menu 
 } from 'antd';
 
-const { SubMenu } = Menu;
-const {
-  Header, Content, Sider,
-} = Layout;
+const {Header} = Layout;
 
 class Navbar extends Component {
   state = {}
   render() {
     return (
       <div>
-        <Layout>
-          <Header className="header">
+        <Layout className="layout">
+          <Header>
             <div className="logo" />
             <Menu
               theme="dark"
@@ -28,43 +25,6 @@ class Navbar extends Component {
               <Menu.Item key="3">nav 3</Menu.Item>
             </Menu>
           </Header>
-          <Layout>
-            <Sider width={200} style={{ height: '100vh', background: '#fff' }}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0 }}
-              >
-                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
-            <Layout style={{ padding: '0 24px 24px' }}>
-              <Content style={{
-                background: '#fff', padding: 24, margin: 0, height: '100vh',
-              }}
-              >
-                Content
-        			</Content>
-            </Layout>
-          </Layout>
         </Layout>
       </div>
     );
