4c09afd0a8637a3d4593485b23e52ca18e024233
Harsh <29hj29@gmail.com>
2019-03-22 18:30:50 +0700

    Add Login and Register Route

--- config/key.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://user:user123@cluster0-m9mff.mongodb.net/test?retryWrites=true"
-}
---
+++ routes/api/login.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/user');
+
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) res.status(400).json({ msg: 'User does not exist' });
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) res.status(400).json({ msg: 'Invalid credentials' });
+
+          jwt.sign(
+            { id: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email
+                }
+              })
+            }
+          )
+
+        })
+    })
+
+})
+
+router.get('/user', auth, (req, res) => {
+  User.findById(req.user.id)
+    .selected('-password')
+    .then(user => res.json(user))
+})
+
+module.exports = router;
