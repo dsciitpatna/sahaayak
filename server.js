1475e6cdb349da6f47bc2b3327b29c4307f19b86
Harsh <29hj29@gmail.com>
2019-03-22 18:30:50 +0700

    Add Login and Register Route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('config');
 
 const app = express();
 
-const db = require('./config/key').mongoURI;
+const db = config.get('mongoURI');
 
+// Connecting to database
 mongoose
     .connect(db, {
         useNewUrlParser: true,
@@ -17,20 +19,19 @@ mongoose
 //set mongoose's Promise equal to global Promise since mongoose's Promise version is depricated
 mongoose.Promise = global.Promise;
 
-app.use(express.static('public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-    res.send('connected');
-})
+// Use routes
+
+app.use('/api/login', require('./routes/api/login'));
+app.use('/api/register', require('./routes/api/register'));
 
 //error handling middleware
 app.use(function (err, req, res, next) {
     res.status(422).send({ error: err.message });
 });
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}`));
