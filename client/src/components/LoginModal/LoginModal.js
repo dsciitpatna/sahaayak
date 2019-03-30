da4c1899fdde5c5dbbd81de22247b60109c3c4ee
ritwizsinha <ritwizsinha0@gmail.com>
2019-03-30 17:00:13 +0530

    Resolved the Warnings on the console

--- client/src/components/LoginModal/LoginModal.js
+++ client/src/components/LoginModal/LoginModal.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import 'antd/dist/antd.css';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { login, closeLoginModal, openRegisterModal } from '../../redux/actions/authActions';
-import { clearErrors } from '../../redux/actions/errorActions';
-import { Modal, Form, Input, Alert } from 'antd';
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import {
+  login,
+  closeLoginModal,
+  openRegisterModal
+} from "../../redux/actions/authActions";
+import { clearErrors } from "../../redux/actions/errorActions";
+import { Modal, Form, Input, Alert } from "antd";
 
 class LoginModal extends Component {
   state = {
     visible: this.props.openloginModal,
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     msg: null
-  }
+  };
 
   static propTypes = {
     isAuthenticated: PropTypes.bool,
@@ -22,25 +27,25 @@ class LoginModal extends Component {
     clearErrors: PropTypes.func.isRequired,
     closeLoginModal: PropTypes.func.isRequired,
     openRegisterModal: PropTypes.func.isRequired
-  }
+  };
 
   componentDidUpdate(prevProps) {
     const { error, isAuthenticated } = this.props;
     if (error !== prevProps.error) {
-      if (error.id === 'LOGIN_FAIL') {
+      if (error.id === "LOGIN_FAIL") {
         this.setState({
           msg: error.msg
-        })
+        });
       } else {
         this.setState({
           msg: null
-        })
+        });
       }
     }
 
     if (this.state.visible) {
       if (isAuthenticated) {
-        console.log('hello');
+        console.log("hello");
         this.toggleModal();
       }
     }
@@ -49,38 +54,38 @@ class LoginModal extends Component {
   toggleModal = () => {
     this.props.clearErrors();
     this.setState({
-      email: '',
-      password: ''
-    })
+      email: "",
+      password: ""
+    });
     this.props.closeLoginModal();
-  }
+  };
 
   handleCancel = () => {
     this.toggleModal();
-  }
+  };
 
   handleCreate = () => {
     const { email, password } = this.state;
     const user = {
       email,
       password
-    }
+    };
 
     this.props.login(user);
-  }
+  };
 
   onChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   toggleRegisterModal = () => {
     this.props.clearErrors();
     this.setState({
-      email: '',
-      password: ''
-    })
+      email: "",
+      password: ""
+    });
     this.props.openRegisterModal();
-  }
+  };
 
   render() {
     const { visible, email, password, msg } = this.state;
@@ -96,13 +101,26 @@ class LoginModal extends Component {
           {msg ? <Alert message={msg} type="error" /> : null}
           <Form layout="vertical">
             <Form.Item label="Email">
-              <Input type="email" name="email" value={email} onChange={this.onChange} />
+              <Input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.onChange}
+              />
             </Form.Item>
             <Form.Item label="Password">
-              <Input type="password" name="password" value={password} onChange={this.onChange} />
+              <Input
+                type="password"
+                name="password"
+                value={password}
+                onChange={this.onChange}
+              />
             </Form.Item>
             {/* Don't have an account? <Link title="Register" /> */}
-            Don't have an account? <a href="#" onClick={this.toggleRegisterModal}>Register</a>
+            Don't have an account?{" "}
+            <Link title="Register" to="#" onClick={this.toggleRegisterModal}>
+              Register
+            </Link>
           </Form>
         </Modal>
       </div>
@@ -114,6 +132,9 @@ const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated,
   error: state.error,
   openloginModal: state.auth.openloginModal
-})
+});
 
-export default connect(mapStateToProps, { login, clearErrors, closeLoginModal, openRegisterModal })(LoginModal);
+export default connect(
+  mapStateToProps,
+  { login, clearErrors, closeLoginModal, openRegisterModal }
+)(LoginModal);
--- client/src/components/RegisterModal/RegisterModal.js
+++ client/src/components/RegisterModal/RegisterModal.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react'
-import 'antd/dist/antd.css';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { Modal, Form, Input, Alert } from 'antd';
-import { register, openLoginModal, closeRegisterModal } from '../../redux/actions/authActions';
-import { clearErrors } from '../../redux/actions/errorActions';
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Modal, Form, Input, Alert } from "antd";
+import {
+  register,
+  openLoginModal,
+  closeRegisterModal
+} from "../../redux/actions/authActions";
+import { clearErrors } from "../../redux/actions/errorActions";
+import { Link } from "react-router-dom";
 
 class RegisterModal extends Component {
   state = {
     visible: this.props.openregisterModal,
-    name: '',
-    email: '',
-    password: '',
+    name: "",
+    email: "",
+    password: "",
     msg: null
-  }
+  };
 
   static propTypes = {
     isAuthenticated: PropTypes.bool,
@@ -23,19 +28,19 @@ class RegisterModal extends Component {
     clearErrors: PropTypes.func.isRequired,
     openLoginModal: PropTypes.func.isRequired,
     closeRegisterModal: PropTypes.func.isRequired
-  }
+  };
 
   componentDidUpdate(prevProps) {
     const { error, isAuthenticated } = this.props;
     if (error !== prevProps.error) {
-      if (error.id === 'REGISTER_FAIL') {
+      if (error.id === "REGISTER_FAIL") {
         this.setState({
           msg: error.msg
-        })
+        });
       } else {
         this.setState({
           msg: null
-        })
+        });
       }
     }
 
@@ -49,16 +54,16 @@ class RegisterModal extends Component {
   toggleModal = () => {
     this.props.clearErrors();
     this.setState({
-      name: '',
-      email: '',
-      password: ''
-    })
+      name: "",
+      email: "",
+      password: ""
+    });
     this.props.closeRegisterModal();
-  }
+  };
 
   handleCancel = () => {
     this.toggleModal();
-  }
+  };
 
   handleCreate = () => {
     const { name, email, password } = this.state;
@@ -66,23 +71,23 @@ class RegisterModal extends Component {
       name,
       email,
       password
-    }
+    };
     this.props.register(newUser);
-  }
+  };
 
   onChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   openLoginModal = () => {
     this.props.clearErrors();
     this.setState({
-      name: '',
-      email: '',
-      password: ''
-    })
+      name: "",
+      email: "",
+      password: ""
+    });
     this.props.openLoginModal();
-  }
+  };
 
   render() {
     const { visible, name, email, password, msg } = this.state;
@@ -101,12 +106,25 @@ class RegisterModal extends Component {
               <Input name="name" value={name} onChange={this.onChange} />
             </Form.Item>
             <Form.Item label="Email">
-              <Input type="email" name="email" value={email} onChange={this.onChange} />
+              <Input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.onChange}
+              />
             </Form.Item>
             <Form.Item label="Password">
-              <Input type="password" name="password" value={password} onChange={this.onChange} />
+              <Input
+                type="password"
+                name="password"
+                value={password}
+                onChange={this.onChange}
+              />
             </Form.Item>
-            Already have an account? <a href="#" onClick={this.openLoginModal}>Login</a>
+            Already have an account?{" "}
+            <Link to="#" onClick={this.openLoginModal}>
+              Login
+            </Link>
           </Form>
         </Modal>
       </div>
@@ -118,6 +136,9 @@ const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated,
   error: state.error,
   openregisterModal: state.auth.openregisterModal
-})
+});
 
-export default connect(mapStateToProps, { register, clearErrors, openLoginModal, closeRegisterModal })(RegisterModal);
+export default connect(
+  mapStateToProps,
+  { register, clearErrors, openLoginModal, closeRegisterModal }
+)(RegisterModal);
