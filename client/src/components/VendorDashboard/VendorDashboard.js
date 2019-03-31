191f1adfe1a9fd7c60aa76781b28090e0d9cf63e
Ashwani <ashyadavash@gmail.com>
2019-03-31 18:36:37 +0530

    added vendor dashboard

---
+++ client/src/components/VendorDashboard/VendorDashboard.js
@@ -0,0 +1,10 @@
+import React, { Component, Fragment } from "react";
+import "antd/dist/antd.css";
+
+class VendorDashboard extends Component {
+  render() {
+      <div><h1>Vendor Dashboard</h1></div>
+  }
+}
+
+export default VendorDashboard;
