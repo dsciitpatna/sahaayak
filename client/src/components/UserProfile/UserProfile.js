324ab719b05a0ef2b43aa59344d53e262898a0e6
Subhang23 <43785080+Subhang23@users.noreply.github.com>
2019-04-01 22:46:08 +0530

    Issue #20 (#32)
    * UserProfile
    
    * Added form to the User profile page

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,14 +1,129 @@
 import React, { Component, Fragment } from "react";
 import "antd/dist/antd.css";
 import { connect } from "react-redux";
+import { Form,Input,Checkbox,Button,Row,Col,Upload, Icon, message } from "antd";
+
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+  const isJPG = file.type === 'image/jpeg';
+  if (!isJPG) {
+    message.error('You can only upload JPG file!');
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!');
+  }
+  return isJPG && isLt2M;
+}
 
 class UserProfile extends Component {
+  state={
+    name:"",
+    email:"",
+    password:"",
+    newpassword:"",
+    phone:"",
+    // Property used for uploading image
+    loading:false
+ }
+ onChange = e => {
+  this.setState({ [e.target.name]: e.target.value });
+  };
+  // Function is used for uploading picture
+  handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, imageUrl => this.setState({
+        imageUrl,
+        loading: false,
+      }));
+    }
+  } 
   render() {
     const { isAuthenticated, user } = this.props;
-    if (isAuthenticated && user.role === "user") {
+    const {name,email,password,newpassword,phone}=this.state;
+    const uploadButton = (
+      <div>
+        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    const imageUrl = this.state.imageUrl;
+    if (isAuthenticated && user.isVendor === false) {
       return (
         <Fragment>
-          <p>User Logged in</p>
+          <Form layout="vertical">
+            <Row>
+              <Col span={8}>
+                <p>Here is a pic</p>
+                <Upload
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploader"
+                  showUploadList={false}
+                  action="//jsonplaceholder.typicode.com/posts/"
+                  beforeUpload={beforeUpload}
+                  onChange={this.handleChange}
+                >
+                  {imageUrl ? <img src={imageUrl} alt="avatar" /> : uploadButton}
+                </Upload>
+              
+              </Col>
+              <Col span={16}>
+              <Form.Item label="Username">
+              <Input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.onChange}
+              />
+            </Form.Item>
+            <Form.Item label="E-mail Id">
+              <Input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.onChange}
+              />            
+            </Form.Item>
+            <Form.Item label="New Password">
+              <Input
+                type="password"
+                name="password"
+                value={password}
+                onChange={this.onChange}
+              />
+            </Form.Item>
+            <Form.Item label="Confirm New Password">
+              <Input
+                type="password"
+                name="newpassword"
+                value={newpassword}
+                onChange={this.onChange}
+              />           
+             </Form.Item>
+            <Form.Item label="Phone Number">
+             <Input
+                type="text"
+                name="phone"
+                value={phone}
+                onChange={this.onChange}
+              />
+            </Form.Item>
+            <Button>UPDATE</Button>
+              </Col>
+            </Row>
+            
+          </Form>
         </Fragment>
       )
     }
