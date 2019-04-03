ef5ad501c504a996530ea3d2cbee5666a1c81c53
Harsh <29hj29@gmail.com>
2019-04-03 20:34:49 +0530

    Remove Server from repo

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { logout } from '../../redux/actions/authActions';
+import { Icon } from 'antd';
+
+class Logout extends Component {
+  static propTypes = {
+    logout: PropTypes.func.isRequired
+  };
+  render() {
+    return (
+      <Link to="/" onClick={this.props.logout}>
+        <Icon type="export" style={{ paddingRight: 10 }} />
+        Logout
+      </Link>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { logout }
+)(Logout);
