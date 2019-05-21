48e0d4ac6bf7f562a7e3744b0651799488b6cf19
Hardik-Agrawal <hardik1509agrawal@gmail.com>
2019-05-21 19:27:14 +0530

    changed top vendor section

--- src/components/topVendors/topvendors.js
+++ src/components/topVendors/topvendors.js
@@ -1,62 +1,72 @@
 import React, { Component } from "react";
 import "./topVendors.css";
 import "antd/dist/antd.css";
-import { Card, Rate } from "antd";
+import { Card, Rate, Row, Col } from "antd";
 
 const { Meta } = Card;
 
 class TopVendors extends Component {
   state = {
-    values: [4.5, 5, 4, 4.5]
+    values: [4.5, 5, 4, 3.5]
   };
 
   render() {
     return (
       <div className="Container">
         <h1>Top Vendors</h1>
-        <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={<img src="https://picsum.photos/200" alt="Name" />}
-        >
-          <div className="Vendor-info">
-            <Meta title="Name" description="Service Name" />
-            <Rate disabled allowHalf value={this.state.values[0]} />
-          </div>
-        </Card>
+        <Row type="flex" justify="center">
+          <Col>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img src="https://picsum.photos/200" alt="Name" />}
+            >
+              <div className="Vendor-info">
+                <Meta title="Name" description="Service Name" />
+                <Rate disabled allowHalf value={this.state.values[0]} />
+              </div>
+            </Card>
+          </Col>
 
-        <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={<img src="https://picsum.photos/200" alt="Name" />}
-        >
-          <div className="Vendor-info">
-            <Meta title="Name" description="Service Name" />
-            <Rate disabled allowHalf value={this.state.values[1]} />
-          </div>
-        </Card>
+          <Col>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img src="https://picsum.photos/200" alt="Name" />}
+            >
+              <div className="Vendor-info">
+                <Meta title="Name" description="Service Name" />
+                <Rate disabled allowHalf value={this.state.values[1]} />
+              </div>
+            </Card>
+          </Col>
 
-        <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={<img src="https://picsum.photos/200" alt="Name" />}
-        >
-          <div className="Vendor-info">
-            <Meta title="Name" description="Service Name" />
-            <Rate disabled allowHalf value={this.state.values[2]} />
-          </div>
-        </Card>
+          <Col>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img src="https://picsum.photos/200" alt="Name" />}
+            >
+              <div className="Vendor-info">
+                <Meta title="Name" description="Service Name" />
+                <Rate disabled allowHalf value={this.state.values[2]} />
+              </div>
+            </Card>
+          </Col>
 
-        <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={<img src="https://picsum.photos/200" alt="Name" />}
-        >
-          <div className="Vendor-info">
-            <Meta title="Name" description="Service Name" />
-            <Rate disabled allowHalf value={this.state.values[3]} />
-          </div>
-        </Card>
+          <Col>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img src="https://picsum.photos/200" alt="Name" />}
+            >
+              <div className="Vendor-info">
+                <Meta title="Name" description="Service Name" />
+                <Rate disabled allowHalf value={this.state.values[3]} />
+              </div>
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
