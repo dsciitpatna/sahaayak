802bc97e91fc0bfedd66807a9756e0a467b8222b
ritwizsinha <ritwizsinha0@gmail.com>
2019-05-22 16:40:27 +0530

    made the changes

--- src/redux/actions/vendorActions.js
+++ src/redux/actions/vendorActions.js
@@ -1,5 +1,6 @@
-import { REGISTER_BUSINESS_FAIL,REGISTER_BUSINESS_SUCCESS } from './type';
+import { REGISTER_BUSINESS_SUCCESS } from './type';
 import axios from "axios";
+import {returnErrors} from './errorActions';
 import { tokenConfig } from './authActions';
 
 export const registerBusiness = ({businessname,description,residence,phone}) => (dispatch, getState) => {
@@ -12,15 +13,14 @@ export const registerBusiness = ({businessname,description,residence,phone}) => 
   const config = tokenConfig(getState);
   axios
     .post("/services", body,config)
-    .then(res =>
+    .then(res =>{
       dispatch({
         type: REGISTER_BUSINESS_SUCCESS,
         payload: res.data
-      })
+      })}
     )
-    .catch(err =>
-      dispatch({
-        type: REGISTER_BUSINESS_FAIL
-      })
+    .catch(err =>{
+      dispatch(returnErrors(err.response.data,err.response.status,'REGISTER_BUSINESS_FAIL'));
+    }
     );
 };
--- src/redux/reducers/vendorReducer.js
+++ src/redux/reducers/vendorReducer.js
@@ -1,31 +1,19 @@
-import {REGISTER_BUSINESS_FAIL,REGISTER_BUSINESS_SUCCESS} from '../actions/type';
+import { REGISTER_BUSINESS_SUCCESS } from '../actions/type';
 
 const initialState = {
-  token: localStorage.getItem('token'),
-  isAuthenticated: false,
-  isLoading: false,
-  user: null,
-  openloginModal: false,
-  openregisterModal: false
+  status: null,
+  msg: ''
 }
 
-export default function(state= initialState,action){
-  switch(action.type){
-  case REGISTER_BUSINESS_FAIL:
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    return {
-      ...state,
-      isLoading: false,
-      isAuthenticated: false,
-      user: null,
-      token: null
-    }
-  case REGISTER_BUSINESS_SUCCESS: 
-  return{
-    ...state,
-    msg: action.payload
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case REGISTER_BUSINESS_SUCCESS:
+      return {
+        ...state,
+        status: action.payload.status,
+        msg: action.payload.msg
+      }
+    default:
+      return state;
   }
-  default:
-  return state;
-}}
+}
