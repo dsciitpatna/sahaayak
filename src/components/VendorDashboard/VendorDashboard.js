7a2740ec1925bfd8bb1a45db3ba8403cf1e18c83
ritwizsinha <ritwizsinha0@gmail.com>
2019-05-24 15:49:29 +0530

    Made the interface of vendor dashboard

--- src/components/VendorDashboard/VendorDashboard.js
+++ src/components/VendorDashboard/VendorDashboard.js
@@ -1,14 +1,57 @@
 import React, { Component, Fragment } from 'react';
 import 'antd/dist/antd.css';
+import './Vendordashboard.css';
 import { connect } from 'react-redux';
+import { Typography, Icon, Row, Col, Button } from 'antd';
+const { Title } = Typography;
 
 class VendorDashboard extends Component {
+  state = {
+    loading: false,
+    iconLoading: false,
+  };
+
+  enterLoading = () => {
+    this.setState({ loading: true });
+  };
+
+  enterIconLoading = () => {
+    this.setState({ iconLoading: true });
+  };
+
   render() {
     const { isAuthenticated, user } = this.props;
     if (isAuthenticated && user.isVendor === true) {
       return (
         <Fragment>
-          <p>Vendor Logged in</p>
+          <div className="container">
+            <Row gutter={300} type="flex" justify="space-around" align="middle">
+              <Col className="gutter-row" span={6}>
+                <img
+                  src="https://images.pexels.com/photos/556416/pexels-photo-556416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=150"
+
+                  alt=""
+                />
+                 <Button
+                  type="primary"
+                  icon="camera"
+                  loading={this.state.iconLoading}
+                  onClick={this.enterIconLoading}
+                >
+                  Change Profile Pic
+               </Button>
+
+              </Col>
+
+              <Col className="gutter-row" span={18}>
+                <div className="gutter-box">
+                  <Title style={{ textAlign: "center" }}>Rajeev Ahuja</Title>
+                  <hr />
+                </div>
+              </Col>
+
+            </Row>
+          </div>
         </Fragment>
       )
     }
