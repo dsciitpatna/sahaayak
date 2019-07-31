0d071144b59395afd0e3ab243ce0c76d4ef35d75
pranaykgupta <pranaykgupta21@gmail.com>
2019-07-31 18:10:40 +0530

    Added margin between top vendors

--- src/components/topVendors/topvendors.js
+++ src/components/topVendors/topvendors.js
@@ -15,7 +15,7 @@ class TopVendors extends Component {
       <div className="Container">
         <h1>Top Vendors</h1>
         <Row type="flex" justify="center">
-          <Col>
+          <Col style={{ margin: '0 20px' }}>
             <Card
               hoverable
               style={{ width: 240 }}
@@ -28,7 +28,7 @@ class TopVendors extends Component {
             </Card>
           </Col>
 
-          <Col>
+          <Col style={{ margin: '0 20px' }}>
             <Card
               hoverable
               style={{ width: 240 }}
@@ -41,7 +41,7 @@ class TopVendors extends Component {
             </Card>
           </Col>
 
-          <Col>
+          <Col style={{ margin: '0 20px' }}>
             <Card
               hoverable
               style={{ width: 240 }}
@@ -54,7 +54,7 @@ class TopVendors extends Component {
             </Card>
           </Col>
 
-          <Col>
+          <Col style={{ margin: '0 20px' }}>
             <Card
               hoverable
               style={{ width: 240 }}
