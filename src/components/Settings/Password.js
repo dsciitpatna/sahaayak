a40a7e5c9b228c90118b317bec29ba57413aea16
ritwizsinha <ritwizsinha0@gmail.com>
2019-08-03 15:17:44 +0530

    Connected the profile page to the backend

--- src/components/Settings/Password.js
+++ src/components/Settings/Password.js
@@ -1,12 +1,45 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import { Input, Row, Col, Button } from 'antd';
-
+import { Input, Row, Col, Button, notification} from 'antd';
+import {updatePassword} from '../../redux/actions/userActions';
 const InputGroup = Input.Group;
 
 class Password extends Component {
+  state ={
+    oldPassword: null,
+    newPassword: null,
+    confNewPassword: null
+  }
+  onChange = e=>{
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  openNotificationWithIcon = (type,message) => {
+    notification[type]({
+      message: message,
+    });
+  };
+  onClick = ()=>{
+    if(this.state.newPassword !== this.state.confNewPassword){
+      return this.openNotificationWithIcon('error','Password doesn\'t match');
+    }
+    else if(this.state.oldPassword === this.state.newPassword){
+      return this.openNotificationWithIcon('info','Cannot set the same password');
+    }
+    else{
+       this.props.updatePassword({oldPassword: this.state.oldPassword,newPassword:this.state.newPassword},this.props.user.id);
+
+    }
+
+  }
   render() {
-    return (
+  if(this.props.passwordChangeStatus === 'Done')   
+    this.openNotificationWithIcon('success','Changed the password successfully') ;
+  else if(this.props.passwordChangeStatus === 'Fail') 
+   this.openNotificationWithIcon('error','Some error occured try again')
+     return (
       <Fragment>
         <h1>Password</h1>
         <hr />
@@ -17,7 +50,7 @@ class Password extends Component {
               <label>Current Password</label>
             </Col>
             <Col span={8}>
-              <Input />
+              <Input name="oldPassword" value={this.state.oldPassword}  type="password"  onChange={this.onChange}/>
             </Col>
           </Row>
         </InputGroup>
@@ -28,10 +61,9 @@ class Password extends Component {
               <label>New Password</label>
             </Col>
             <Col span={8}>
-              <Input />
+              <Input name="newPassword" value={this.state.newPassword} type="password" onChange={this.onChange}/>
             </Col>
-          </Row>
-        </InputGroup>
+          </Row>        </InputGroup>
         <br />
         <InputGroup>
           <Row gutter={8}>
@@ -39,7 +71,7 @@ class Password extends Component {
               <label>Verify Password</label>
             </Col>
             <Col span={8}>
-              <Input />
+              <Input name="confNewPassword" value={this.state.confNewPassword} type="password" onChange={this.onChange}/>
             </Col>
           </Row>
         </InputGroup>
@@ -48,14 +80,17 @@ class Password extends Component {
           <div>Forgot your Password?</div>
         </InputGroup>
         <br />
-        <Button type="primary">
+        <Button type="primary" onClick={this.onClick} loading={this.state.loading}>
           Save Changes
         </Button>
       </Fragment>)
   }
 }
-
+const mapStateToProps = state=>({
+  user: state.user.user,
+  passwordChangeStatus: state.user.passwordChangeStatus
+})
 export default connect(
-  null,
-  null
+  mapStateToProps,
+  {updatePassword}
 )(Password);
